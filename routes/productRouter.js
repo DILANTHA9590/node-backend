f4392bf353a5ce1fb1525eaaefd945f829a089e3
DILANTHA9590 <dilanthanayanajith@gmail.com>
2024-12-15 11:41:53 +0530

    create product  router

---
+++ routes/productRouter.js
@@ -0,0 +1,16 @@
+import express, { Router } from "express"
+
+
+import { createNewProduct,getProduct } from "../controller/productcontroller.js"
+
+
+const productRouter = express.Router();
+
+
+productRouter.post("/",createNewProduct);
+
+productRouter.get("/", getProduct);
+
+
+
+export default productRouter
