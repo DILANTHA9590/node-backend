eff479cb3077bf473fc8b2a1b229f8b53a3592c1
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-25 00:02:36 +0530

    🌐✨ Added Google login route to handle user authentication via Google 🚀🔑

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,7 @@ import {
   userCreation,
   getuser,
   loginUser,
+  googleLogin,
 } from "../controller/userController.js";
 
 const userRouter = express.Router();
@@ -12,5 +13,7 @@ userRouter.post("/", userCreation);
 userRouter.get("/", getuser);
 
 userRouter.post("/login", loginUser);
+userRouter.post("/google", googleLogin);
+
 
 export default userRouter;
