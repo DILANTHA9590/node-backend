f746647828f5f96e3ddbeced4b9445e9b50d0de1
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-28 13:47:20 +0530

    Add route to update orders by order ID 🚀

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, getOrderList, getQuotes } from "../controller/orderController.js";
+import { createOrder, getOrderList, getQuotes, updateOrder } from "../controller/orderController.js";
 
 
 const orderRouter = express.Router();
@@ -9,6 +9,8 @@ const orderRouter = express.Router();
 orderRouter.post("/",createOrder);
 orderRouter.get("/",getOrderList);
 orderRouter.post("/quote",getQuotes);
+
+orderRouter.put("/:orderId",updateOrder);
 
 
 export default orderRouter
