6139bb84ab79188a63b37d8c060f0996d39a9d14
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-29 12:10:03 +0530

    🚀 Add '/search/:query' route for product search

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,7 +1,7 @@
 import express from "express"
 
 
-import { createNewProduct,deleteProduct,getProduct, getProductById, updateProduct } from "../controller/productcontroller.js"
+import { createNewProduct,deleteProduct,getProduct, getProductById, searchProduct, updateProduct } from "../controller/productcontroller.js"
 
 
 const productRouter = express.Router();
@@ -10,6 +10,8 @@ const productRouter = express.Router();
 productRouter.post("/",createNewProduct);
 
 productRouter.get("/", getProduct);
+
+productRouter.get("/search/:query", searchProduct);
 
 productRouter.get("/:productId", getProductById);
 productRouter.delete("/:productId", deleteProduct);
