9375501a05ee6408f07c11b7785485dbb488f5a7
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-02-23 08:25:47 +0530

    add order count route

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, getOrderList, getQuotes, updateOrder } from "../controller/orderController.js";
+import { createOrder, getOrderList, getQuotes, orderCount, updateOrder } from "../controller/orderController.js";
 
 
 const orderRouter = express.Router();
@@ -9,6 +9,8 @@ const orderRouter = express.Router();
 orderRouter.post("/",createOrder);
 orderRouter.get("/",getOrderList);
 orderRouter.post("/quote",getQuotes);
+orderRouter.get("/ordercount",orderCount)
+
 
 orderRouter.put("/:orderId",updateOrder);
 
